4a392f7863466371d5cda422b8419e7fa50baa30
YamilaGermanier <yamiger91@gmail.com>
2024-03-31 22:33:26 -0300

    fibonacci

--- ejercicio01.py
+++ ejercicio01.py
@@ -1,2 +1,13 @@
 # 1 - Implementar una función que permita obtener el valor en la sucesión de Fibonacci para un número dado.
 
+# 1 1 2 3 5 8 13... n=n-1 + n-2
+def fibonacci(num):
+    if num == 1 or num == 0:
+        return num
+    else:
+        return fibonacci(num-1) + fibonacci(num-2)
+    
+
+num=8
+
+print(fibonacci(num))
